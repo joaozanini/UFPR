814b7ff9f39fc1e28a4a25a6ed92affaa3ceb303
João Vitor <joaozanini1@ufpr.br>
2025-04-08 20:23:23 -0300

    Create charPosition.c

---
+++ LinguagensDeProgramação/charPosition.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{   
+    char str[100];
+    char caractere;
+    
+    fgets(str, sizeof(str), stdin);
+    scanf("%s", &caractere);
+    
+    for (int i = 0; i <= sizeof(str); i++) {
+        if (str[i] == caractere) {
+            printf("%d", i);
+            break;
+        } else if (str[i] == '\0') {
+            printf("%d", -1);
+            break;
+        }
+    }
+    
+    return 0;
+}
