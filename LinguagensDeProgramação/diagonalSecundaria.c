9d519c49c945fa712b940b9cfc21516f3f93117b
João Vitor <joaozanini1@ufpr.br>
2025-04-22 11:57:03 -0300

    Create diagonalSecundaria.c

---
+++ LinguagensDeProgramação/diagonalSecundaria.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int matriz[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        printf("%d\n", matriz[i][2 - i]);
+    }
+
+    return 0;
+}
