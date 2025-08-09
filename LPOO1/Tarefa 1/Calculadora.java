977ab0c2720d1ce1f93d7b4e4679ef09cca3b5a0
João Zanini <joaozanini.pessoal@gmail.com>
2025-08-09 00:09:54 -0300

    .

---
+++ LPOO1/Tarefa 1/Calculadora.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        System.out.println("<número 1> <número 2> <operação>");
+        Scanner sc = new Scanner(System.in);
+
+        double resultado;
+        String input =  sc.nextLine();
+
+        String[] op = input.split(" ");
+        double num1 = Double.parseDouble(op[0]);
+        double num2 = Double.parseDouble(op[1]);
+        String operacao = op[2];
+
+        switch (operacao) {
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "*":
+                resultado = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0) {
+                    System.out.println("Erro: divisão por zero!");
+                    return;
+                }
+                resultado = num1 / num2;
+                break;
+            default:
+                System.out.println("Operação inválida! Use +, -, * ou /");
+                return;
+        }
+
+        System.out.println(resultado);
+    }
+}
